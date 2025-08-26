8019db2513d8ee03c421ee2fc1352ece2bb55cb9
Patricio-Salazar <pvssalcedo2502@gmail.com>
2025-08-26 17:10:53 -0300

    Proyecto inicial InstitutoCristoRey C# .NET

---
+++ DOMINIO/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMINIO
+{
+    public class alumno
+    {
+        //aca voy a ir metiendo los atributos
+        public int dni { get; set; }
+
+    }
+}
