fe17b12ecd12a08ae4eadc745d48890d8a717e36
Patricio-Salazar <pvssalcedo2502@gmail.com>
2025-08-29 15:58:05 -0300

    listo el acceso a datos y el el acceso a datos para alumno esta a la mitad

--- DOMINIO/Alumno.cs
+++ DOMINIO/Alumno.cs
@@ -11,7 +11,7 @@ namespace DOMINIO
         //aca voy a ir metiendo los atributos
         public int dni { get; set; }
         public string nombre { get; set; }
-        public bool egresado {  get; set; }
+        public string apellido { get; set; }
 
     }
 }
